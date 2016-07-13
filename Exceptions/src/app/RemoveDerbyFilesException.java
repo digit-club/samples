1ce22a2f65fea1db68f1bcfeba9edf0a5a104a60
Paul Isaichuk <pisaychuk@hortonworks.com>
2016-07-13 16:47:07 +0300

    Exceptions example

---
+++ Exceptions/src/app/RemoveDerbyFilesException.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.io.IOException;
+
+public class RemoveDerbyFilesException extends IOException{
+
+    public RemoveDerbyFilesException() {
+        super();
+    }
+
+    public RemoveDerbyFilesException(Exception cause) {
+        super(cause);
+    }
+
+}
