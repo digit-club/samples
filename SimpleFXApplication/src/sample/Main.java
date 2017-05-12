25cc724563efa4112100a33e01a26cd8dc915fa0
Your Name <you@example.com>
2017-05-12 21:06:13 +0300

    digit UI sample inhancement

--- SimpleFXApplication/src/sample/Main.java
+++ SimpleFXApplication/src/sample/Main.java
@@ -17,6 +17,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.CopyOption;
 import java.nio.file.FileSystemException;
@@ -40,54 +41,85 @@ public class Main extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        Text scenetitle = new Text("Please specify paths:");
-        grid.add(scenetitle, 0, 0/*, 2, 1*/);
+        Text title = new Text("Please specify paths:");
+        grid.add(title, 0, 0/*, 2, 1*/);
 
-        Label userName = new Label("Source:");
-        grid.add(userName, 0, 1);
+        Label source = new Label("Source:");
+        grid.add(source, 0, 1);
         TextField sourceTextField = new TextField();
         grid.add(sourceTextField, 1, 1);
 
-        Label pw = new Label("Target:");
-        grid.add(pw, 2, 1);
+        Label target = new Label("Target:");
+        grid.add(target, 2, 1);
         TextField targetTextField = new TextField();
         grid.add(targetTextField, 3, 1);
 
-        Button btn = new Button("Copy");
+        Button btnCopy = new Button("Copy");
+        Button btnCancel = new Button("Cancel");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_CENTER);
-        hbBtn.getChildren().add(btn);
+        hbBtn.getChildren().add(btnCopy);
+        hbBtn.getChildren().add(btnCancel);
         grid.add(hbBtn, 5, 1);
 
-        final Text actiontarget = new Text();
-        btn.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
-            @Override
-            public void handle(MouseEvent e) {
-                String source = sourceTextField.getText();
-                String target = targetTextField.getText();
-                System.out.println("Source : " + source);
-                System.out.println("Target : " + target);
-                try {
-                    Files.copy(Paths.get(source), Paths.get(target));
+        final Text resultMessage = new Text();
 
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                    actiontarget.setFill(Color.FIREBRICK);
-                    actiontarget.setText("Error: " + exception.getClass() + ": " + exception.getMessage());
-                    return;
-                }
-                actiontarget.setFill(Color.GREEN);
-                actiontarget.setText("File was copied.");
+        btnCopy.setOnMouseClicked(e -> {
+            String sourcePath = sourceTextField.getText();
+            String targetPath = targetTextField.getText();
+            System.out.println("Source : " + sourcePath);
+            System.out.println("Target : " + targetPath);
+            try {
+                validate(sourcePath, targetPath);
+                // TODO update target path if directory
+                Files.copy(Paths.get(sourcePath), Paths.get(targetPath));
+                resultMessage.setFill(Color.GREEN);
+                resultMessage.setText("File was copied.");
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                resultMessage.setFill(Color.RED);
+                resultMessage.setText("Error: " + exception.getClass() + ": " + exception.getMessage());
+            } catch (ValidationException validationException) {
+                resultMessage.setFill(Color.RED);
+                resultMessage.setText(validationException.getMessage());
             }
         });
+
+        btnCancel.setOnMouseClicked(e -> {
+            sourceTextField.clear();
+            targetTextField.clear();
+        });
+
         VBox vBox = new VBox();
         vBox.getChildren().add(grid);
-        vBox.getChildren().add(actiontarget);
+        vBox.getChildren().add(resultMessage);
 
         StackPane rootPane = new StackPane();
         rootPane.getChildren().add(vBox);
-        Scene scene = new Scene(rootPane/*, 300, 275*/);
+
+        Scene scene = new Scene(rootPane);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void validate(String source, String target) throws ValidationException {
+        File sourceFile = new File(source);
+        if (!sourceFile.exists()) {
+            throw new ValidationException("Source file doesn't exist!");
+        } else if (sourceFile.isDirectory()) {
+            throw new ValidationException("Source is not a file!");
+        }
+    }
+
+    private class ValidationException extends Exception {
+        private String message;
+
+        public ValidationException(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
     }
 }
