06ebdbef71cfecc1d7070f04d6f22402ddca6371
Paul Isaichuk <pisaychuk@hortonworks.com>
2017-07-21 17:23:18 +0300

    servlets example

--- Servlets/src/servlets/MyExampleServlet.java
+++ Servlets/src/servlets/MyExampleServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +12,18 @@ import java.io.PrintWriter;
 public class MyExampleServlet extends HttpServlet{
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter pw = response.getWriter();
-        pw.print("<h1>Hello Guys!</h1>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/resources/index.html");
+        view.forward(request, response);
     }
 
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+
+        pw.printf("<h1>Hello %s!</h1>\n", request.getParameter("name"));
+        pw.printf("<h3>You are %s years old!</h1>\n", request.getParameter("age"));
+
+    }
 }
